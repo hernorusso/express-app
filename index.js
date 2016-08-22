c1fde39219c87cad1244904396395292a5dddec6
herno <russo.hernan@gmail.com>
2016-08-22 01:17:21 -0300

    Render with Jade. Bind app to mysql Database

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 var express = require('express');
+var mysql = require('mysql');
 var app = express();
 
+// Aplication Settings
 app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+// Configuration
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'secret',
+  database : 'express'
+});
 
 app.get('/', function(req, res) {
-  res.sendFile( __dirname + '/index.html');
+  connection.connect();
+
+  connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+
+    res.render('index', {title: 'Glo', solution: rows[0].solution});
+  });
+
+  connection.end();
+
 });
 
 var server = app.listen(3000, function(){
